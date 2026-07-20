554470eeae46614a4460a62ff46ecf62b7a3610a
Dilli <chdilli200@gmail.com>
2026-07-20 23:14:38 +0530

    Completed Version 2 responsive design

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 console.log("Bio Medical Website Loaded Successfully");
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+    card.addEventListener("mousemove", (e) => {
+
+        const rect = card.getBoundingClientRect();
+
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const rotateY = (x / rect.width - 0.5) * 12;
+        const rotateX = -(y / rect.height - 0.5) * 12;
+
+        card.style.transform =
+        `perspective(1000px)
+        rotateX(${rotateX}deg)
+        rotateY(${rotateY}deg)
+        scale(1.04)`;
+
+    });
+
+    card.addEventListener("mouseleave", () => {
+
+        card.style.transform =
+        "perspective(1000px) rotateX(0) rotateY(0)";
+
+    });
+
+});
